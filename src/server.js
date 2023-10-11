3daf6e47edc710c5101e5e2988fc811d19bdb2a4
Stefan Niemeyer <stefan.niemeyer@vkb.de>
2023-10-11 17:05:34 +0200

    refactoring

--- src/server.js
+++ src/server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 const APP_VERSION = process.env.APP_VERSION || 'unknown';
 const APP_PICTURE = process.env.APP_PICTURE || 'standard.jpg';
 
 const host = process.env.HOSTNAME || 'localhost';
 const PATH_READY = '/ready';
 const PATH_HEALTH = '/health';
+const PATH_ERROR = '/error';
 const startTime = new Date();
 
 let totalRequests = 0;
@@ -32,21 +33,24 @@ const createResponse = (path, method = 'GET') => {
 app.use(express.json());
 
 app.get('/', (_req, res) => {
+  if (!healthState) {
+    return res.status(500).send({status: "Internal Server Error"})
+  }
+  if (!readyState) {
+    return res.status(503).send({status: "Service Unavailable"})
+  }
   totalRequests += 1;
-  return res.send(createResponse('/'))
+  res.send(createResponse('/'))
 });
 
 app.get('/image', (req, res) => {
-  // Get the absolute path to your cat image
-  const catImagePath = path.join(__dirname, 'images', APP_PICTURE);
-
-  // Send the cat image as a response
-  res.sendFile(catImagePath);
+  const imagePath = path.join(__dirname, 'images', APP_PICTURE);
+  res.sendFile(imagePath);
 });
 
 app.get(PATH_READY, (_, res) => {
   const statusCode = readyState ? 200 : 503;
-  res.status(statusCode).send(createResponse(PATH_READY));
+  res.status(statusCode).send({ready: readyState, statusCode: statusCode});
 });
 
 app.post(PATH_READY, (_, res) => {
@@ -60,8 +64,8 @@ app.delete(PATH_READY, (_, res) => {
 })
 
 app.get(PATH_HEALTH, (_, res) => {
-  const statusCode = healthState ? 200 : 503;
-  res.status(statusCode).send(createResponse(PATH_HEALTH));
+  const statusCode = healthState ? 200 : 500;
+  res.status(statusCode).send({health: healthState, statusCode: statusCode});
 });
 
 app.post(PATH_HEALTH, (_, res) => {
@@ -74,8 +78,9 @@ app.delete(PATH_HEALTH, (_, res) => {
   res.send(createResponse(PATH_HEALTH, 'DELETE'))
 });
 
-app.get('/error', () => {
-  console.log(`Host: ${host} | Path: /error | Total Requests: ${totalRequests} | Health: ${healthState} | Ready: ${readyState} | App Uptime: ${(new Date() - startTime)/1000} seconds | Log Time: ${new Date()}`);
+app.get(PATH_ERROR, (_, res) => {
+  console.log(`Host: ${host} | Path: ${PATH_ERROR} | Total Requests: ${totalRequests} | Health: ${healthState} | Ready: ${readyState} | App Uptime: ${(new Date() - startTime)/1000} seconds | Log Time: ${new Date()}`);
+  res.status(503).send({status: "Unexpected Error Occurred"})
   process.exit(1);
 });
 
